3d56660104cb13d7d629ee5d3be1f88e804ac678
zeyad ayman elmaghrabi <161942240+ziadelmaghrabi@users.noreply.github.com>
2026-03-11 00:10:16 +0200

    Refactor smooth scrolling and typing animation
    Updated typing animation text and improved scrolling functionality.

--- hub1.js
+++ hub1.js
@@ -13,7 +13,7 @@ document.querySelectorAll('a[href^="#"]').forEach(link => {
 
 // Typing Animation
 const text =
-  "my name is Ziad Ayman Abdalla Elmaghraby, a passionate Front-End Developer skilled in HTML, CSS, JavaScript, and React. I also have strong experience in C++, Java, Python, and OOP.I enjoy building clean, responsive, and user-friendly interfaces, and I’m always looking to learn new technologies to improve my work. I have a strong problem-solving mindset and love turning ideas into real, functional projects. I am constantly developing my skills through personal projects, teamwork, and continuous learning.";
+  "My name is Ziad Ayman Abdalla Elmaghraby, and I am a passionate Full-Stack Developer with strong skills in Front-End development, including HTML, CSS, JavaScript, and React. I also have experience in Back-End development using C# and SQL, allowing me to build complete and functional web applications.In addition, I have solid programming experience in C++, Java, and Python, with a strong foundation in Object-Oriented Programming (OOP).I enjoy building clean, responsive, and user-friendly interfaces, while also working on the server-side logic and database management to create reliable and efficient systems. I have a strong problem-solving mindset and enjoy turning ideas into real, functional projects.I am always eager to learn new technologies and improve my development skills, and I continuously grow through personal projects, teamwork, and continuous learning.";
 let i = 0;
 const typingText = document.getElementById("typing-text");
 const aboutSection = document.querySelector(".about-container");
@@ -58,3 +58,4 @@ img.addEventListener('mouseleave', () => {
   img.style.setProperty('--rotateY', '0deg');
   img.style.setProperty('--rotateX', '0deg');
 });
+
